3979a1b612bb93a2aa47eef347a3d77d790c40c9
Tin Goricki <exibodude@gmail.com>
2023-02-26 20:25:17 +0100

    Works for multiple words

--- sub-strings.rb
+++ sub-strings.rb
@@ -3,15 +3,21 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings ( word, array_of_words )
     substring_hash = Hash.new
+    word_array = word.split(" ")
 
-
-    array_of_words.each do |string|
-        if word.include?(string)
-            substring_hash[string] = 1
+    word_array.each do |string_in_word_array|
+        array_of_words.each do |string|
+            if string_in_word_array.downcase.include?(string)
+                if substring_hash[string].to_i > 1 || substring_hash[string] == 1
+                    substring_hash[string] += 1
+                else
+                    substring_hash[string] = 1
+                end
         end
     end
-
+end
     puts substring_hash
 end
 
+substrings("Howdy partner, sit down! How's it going?", dictionary);
 substrings("below", dictionary)
